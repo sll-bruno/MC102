cd0776688e902c2f8368fdf8ccd71ffd7d418743
Bruno Salles <brunosalles574@gmail.com>
2024-06-14 13:01:48 -0300

    final version

--- labs/lab08.py
+++ labs/lab08.py
@@ -2,11 +2,12 @@ def main():
     linha,coluna = map(int, input().split(','))
     area = encontrar_area(linha)
     arvores_candidatas = parte_um(linha,coluna,area)
-    dict_candidatas, arvore_escolhida = parte_dois(arvores_candidatas,area)
-    print('Arvores candidatas:')
-    print(*arvores_candidatas,sep='\n')
+    dict_candidatas, arvore_escolhida = parte_dois(arvores_candidatas,area,linha,coluna)
+    print('Árvores candidatas:')
+    for items in arvores_candidatas:
+        print(f'{items[0]},{items[1]}')
     print('Árvore escolhida:')
-    print(f'{arvore_escolhida} com {dict_candidatas[arvore_escolhida]} árvores visíveis')
+    print(f'{arvore_escolhida[0]},{arvore_escolhida[1]} com {dict_candidatas[arvore_escolhida]} árvores visíveis.')
     
 def encontrar_area(linha): #Gera uma matriz cujas linhas são formadas pelo input do usuário
     area = []
@@ -18,118 +19,111 @@ def parte_um(linha,coluna,area): #Retorna a posição das arvores que não são 
     arvores_candidatas =  [(i,j) for i in range(linha) for j in range(coluna) if any(area[i][k] >= area[i][j] for k in range(j)) and any(area[i][k] >= area[i][j] for k in range(j + 1,coluna)) and any(area[k][j] >= area[i][j] for k in range(i)) and any(area[k][j] >= area[i][j] for k in range(i + 1,linha))]
     return arvores_candidatas
 
-def parte_dois(arvores_candidatas,area):
+def parte_dois(arvores_candidatas,area,linha,coluna):
     dict_candidatas = {i:0 for i in arvores_candidatas}
     for arvore in dict_candidatas.keys():
         x = arvore[0]
         y = arvore[1]
-        h = area[x][y]
-        a = 1
+        h = area[x][y] #Armazena a altura da árvore candidata à observatório
 
         #Norte
         try:
-            while h < area[x - a][y]:
-                dict_candidatas[arvore] += 1
-                a += 1
-                if area[x-a][y] >= h:
+            for a in range(1,max([linha,coluna])):
+                if x - a < 0:
+                    break
+                elif area[x - a][y] >= h:
                     dict_candidatas[arvore] += 1
                     break
-        except IndexError:
+                elif h > area[x - a][y]:
+                    dict_candidatas[arvore] += 1
+        except IndexError: 
             pass
-        finally:
-            dict_candidatas[arvore] += 1
 
         #Sul
         try:
-            while h < area[x + a][y]:
-                dict_candidatas[arvore] += 1
-                a += 1
-                if area[x-a][y] >= h:
+            for a in range(1,max([linha,coluna])):
+                if area[x + a][y] >= h:
                     dict_candidatas[arvore] += 1
                     break
-        except IndexError:
+                elif h > area[x + a][y]:
+                    dict_candidatas[arvore] += 1
+        except IndexError: 
             pass
-        finally:
-            dict_candidatas[arvore] += 1   
 
 
-        #Oeste
+        #Oeste -- 
         try:
-            while h < area[x][y + a]:
-                dict_candidatas[arvore] += 1
-                a += 1
-                if area[x-a][y] >= h:
+            for a in range(1,max([linha,coluna])):
+                if h > area[x][y + a]:
+                    dict_candidatas[arvore] += 1
+                elif area[x][y + a] >= h:
                     dict_candidatas[arvore] += 1
                     break
-        except IndexError:
+        except IndexError: 
             pass
-        finally:
-            dict_candidatas[arvore] += 1
 
-        #Leste
+        #Leste --
         try:
-            while h < area[x][y - a]:
-                dict_candidatas[arvore] += 1
-                a += 1
-                if area[x-a][y] >= h:
+            for a in range(1,max([linha,coluna])):
+                if y - a < 0:
+                    break
+                elif area[x][y - a] >= h:
                     dict_candidatas[arvore] += 1
                     break
-        except IndexError:
+                elif h > area[x][y - a]:
+                    dict_candidatas[arvore] += 1
+        except IndexError: 
             pass
-        finally:
-            dict_candidatas[arvore] += 1
 
-        #Nordeste
+        #Nordeste --
         try:
-            while h < area[x - a][y + a]:
-                dict_candidatas[arvore] += 1
-                a += 1
-                if area[x-a][y] >= h:
+            for a in range(1,max([linha,coluna])):
+                if x - a < 0:
+                    break
+                elif area[x - a][y + a] >= h:
                     dict_candidatas[arvore] += 1
                     break
-        except IndexError:
+                elif h > area[x - a][y + a]:
+                    dict_candidatas[arvore] += 1
+        except IndexError: 
             pass
-        finally:
-            dict_candidatas[arvore] += 1
 
-        #Noroeste
+        #Noroeste --
         try:
-            while h < area[x - a][y - a]:
-                dict_candidatas[arvore] += 1
-                a += 1
-                if area[x-a][y] >= h:
+            for a in range(1,max([linha,coluna])):
+                if x-a < 0 or y - a < 0:
+                    break
+                if area[x - a][y - a] >= h:
                     dict_candidatas[arvore] += 1
                     break
-        except IndexError:
-            pass
-        finally:
-            dict_candidatas[arvore] += 1        
+                elif h > area[x - a][y - a]:
+                    dict_candidatas[arvore] += 1
+        except IndexError: 
+            pass      
 
-        #Sudeste
+        #Sudeste --
         try:
-            while h < area[x + a][y + a]:
-                dict_candidatas[arvore] += 1
-                a += 1
-                if area[x-a][y] >= h:
+            for a in range(1,max([linha,coluna])):
+                if area[x + a][y + a] >= h:
                     dict_candidatas[arvore] += 1
                     break
-        except IndexError:
-            pass
-        finally:
-            dict_candidatas[arvore] += 1
+                elif h > area[x + a][y + a]:
+                    dict_candidatas[arvore] += 1
+        except IndexError: 
+            pass   
 
-        #Sudoeste
+        #Sudoeste -- 
         try:
-            while h < area[x + a][y - a]:
-                dict_candidatas[arvore] += 1
-                a += 1
-                if area[x-a][y] >= h:
+            for a in range(1,max([linha,coluna])):
+                if y - a < 0:
+                    break
+                elif area[x + a][y - a] >= h:
                     dict_candidatas[arvore] += 1
                     break
-        except IndexError:
-            pass
-        finally:
-            dict_candidatas[arvore] += 1
+                elif h > area[x + a][y - a]:
+                    dict_candidatas[arvore] += 1
+        except IndexError: 
+            pass   
     
     #Seleciona, do dicionário, a árvore com maior valor
     arvore_escolhida = [arvore[0] for arvore in dict_candidatas.items() if max(dict_candidatas.values()) == arvore[1]]
